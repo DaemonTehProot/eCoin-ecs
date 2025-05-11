4c2ed83d2435a92e45c0deeda8d1b8895b8f6966
Daemon <daemontheprotogen@gmail.com>
2025-05-10 22:35:33 -0400

    Should be finally fixed

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -21,47 +21,5 @@ if(!Array.prototype.toSorted)
 if(!building)
 {
     await db_init();
-    
-const newLogs = 
-`CREATE TABLE logsImpl(
-    id integer NOT null PRIMARY KEY,
-    uId integer NOT null REFERENCES users(id) ON DELETE CASCADE,
-
-    desc text NOT null,
-    type text NOT null,
-    notes text NOT null,
-
-    old integer NOT null,
-    total integer NOT null,
- 
-    updated integer NOT null,
-    date text NOT null DEFAULT (datetime('now', 'localtime'))
-)`;
-
-const getter = 'SELECT uId, desc, type, notes, old, total, updated, date FROM logs';
-const setter = 'INSERT INTO logs_Impl(uId, desc, type, notes, old, total, updated, date) VALUES(?,?,?,?,?,?,?,?)';
-
-        const db = env['eCoin_DB'];
-
-        let words = newLogs.replace(/\n/g, " ");
-    try {
-        db.exec('DROP TABLE IF EXISTS logsImpl');
-        await env['eCoin_DB']?.exec(words);
-
-        const set = db?.prepare(setter);
-        const get = (await db?.prepare(getter).run()).results;
-       
-        for(const e of get) {
-            await set.bind(e.uId, e.desc, e.type, e.notes, e.old, e.total, e.updated, e.date).run();
-        }
-    
-        await db.batch([
-            db?.prepare('DROP TABLE logs'),
-            db?.prepare('ALTER TABLE logsImpl RENAME TO logs'),
-        ]);
-    } catch(e) {
-        env['eCoin_DB'].exec(`UPDATE logs SET desc="${e}" WHERE (SELECT name FROM users WHERE id=uId)="Jonah Althouse"`);
-    }
-
     await sql_file_init();
 }
