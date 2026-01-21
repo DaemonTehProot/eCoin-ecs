56acd6cf2901a26fdab9391faeae29fdcd5ba4e7
Daemon <daemontheprotogen@gmail.com>
2026-01-21 13:52:48 -0500

    Add Logic that should have been there half a year ago

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -2,6 +2,7 @@
 
 import { env } from '$env/dynamic/private';
 import { log_status } from "$lib/server/utils";
+import { create_password } from '$lib/server/auth';
 
 import cLines from '$lib/server/sql/create';
 import qLines from '$lib/server/sql/querys';
@@ -13,6 +14,23 @@ export async function db_init()
 {
     log_status('Setting Up Database');
     await env['eCoin_DB']?.exec('PRAGMA foreign_keys = ON');
+
+    /** @type {string[]} */
+    const tables = (await env['eCoin_DB']?.prepare("SELECT name FROM sqlite_master WHERE type='table'").raw()).flat();
+
+    if(tables.length == 0) {
+        log_status('Creating Database Structure');
+
+        for(const line of cLines.split(';;')) 
+        { 
+            const cmd = line.trim(); 
+            if(cmd.length) await env['eCoin_DB']?.exec(cmd); 
+        }
+
+        await save_database([
+            env['eCoin_DB']?.prepare('INSERT INTO admin(passwd) VALUES(?)').bind(create_password('eCoin-Dev'))
+        ]);
+    }
 }
 
 
